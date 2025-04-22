bff8a502501d25bf6d4c5abdd60efa56965ce7c9
infinitin007 <nitin.agiwal05@gmail.com>
2025-04-22 22:29:15 +0530

    interrupts.h

--- interrupts/interrupts.h
+++ interrupts/interrupts.h
@@ -8,16 +8,12 @@
 
 typedef void (*interrupt_handler_t)();
 
-// Register a handler for a specific interrupt.
 void register_interrupt_handler(uint8_t n, interrupt_handler_t handler);
 
-// Called by the IRQ assembly stubs via the common handler.
 void irq_handler(uint8_t irq);
 
-// New common IRQ handler called from assembly.
 void common_irq_handler(uint32_t irq_num);
 
-// Install IRQ entries in the IDT (for IRQ0–IRQ15).
 void irq_install();
 
 #endif
