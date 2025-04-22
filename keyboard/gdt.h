cc72a7501c0ba37d5d99aaef8870b2c2a49698e4
infinitin007 <nitin.agiwal05@gmail.com>
2025-04-22 22:32:43 +0530

    Update gdt.h

--- keyboard/gdt.h
+++ keyboard/gdt.h
@@ -5,18 +5,18 @@
 
 struct gdt_entry
 {
-    uint16_t limit_low;  // Lower 16 bits of limit.
-    uint16_t base_low;   // Lower 16 bits of base.
-    uint8_t base_middle; // Next 8 bits of base.
-    uint8_t access;      // Access flags.
-    uint8_t granularity; // Granularity flags and high 4 bits of limit.
-    uint8_t base_high;   // Last 8 bits of base.
+    uint16_t limit_low;  
+    uint16_t base_low;   
+    uint8_t base_middle;
+    uint8_t access;     
+    uint8_t granularity; 
+    uint8_t base_high;   
 } __attribute__((packed));
 
 struct gdt_ptr
 {
-    uint16_t limit; // Limit of GDT.
-    uint32_t base;  // Base address of GDT.
+    uint16_t limit; 
+    uint32_t base;  
 } __attribute__((packed));
 
 void gdt_install();
