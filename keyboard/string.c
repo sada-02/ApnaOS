80ed4845133fb651a1eaab29f48d9ce0882b8f2e
Kavy Vaghela <tishyakavy@gmail.com>
2025-04-23 07:32:38 +0530

    Changed string.c strncmp works properly

--- keyboard/string.c
+++ keyboard/string.c
@@ -33,7 +33,7 @@ int strncmp(const char* s1, const char* s2, size_t n) {
         s1++;
         s2++;
     }
-    return 0;
+    return len1 - len2;
 }
 
 char* strncpy(char* dest, const char* src, size_t n) {
