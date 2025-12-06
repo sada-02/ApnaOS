708394a2d64cf3450daa9b34dfef07483d6c1f5e
sada-02 <kartiksarda.2005@gmail.com>
2025-12-06 13:07:03 +0530

    history checking

--- keyboard/keyboard.c
+++ keyboard/keyboard.c
@@ -9,11 +9,17 @@ extern void print_to_screen(const char* message);
 #define KBD_DATA_PORT 0x60
 #define KBD_STATUS_PORT 0x64
 #define KBD_SCANCODE_RELEASE 0x80
+#define HISTORY_SIZE 20
 
 static char input_buffer[BUFFER_SIZE];
 volatile int input_ready = 0;
 volatile char input_line[BUFFER_SIZE];
 static int buffer_index = 0;
+
+static char command_history[HISTORY_SIZE][BUFFER_SIZE];
+static int history_count = 0;
+static int history_index = -1;
+static int browsing_history = 0;
 
 static const char scancode_to_ascii[128] = {
     0, 27, '1', '2', '3', '4', '5', '6',
@@ -46,6 +52,26 @@ static const char scancode_to_ascii_shift[128] = {
 int shift_pressed = 0;
 int caps_lock_on = 0;
 
+void clear_current_input() {
+    while (buffer_index > 0) {
+        buffer_index--;
+        print_to_screen("\b \b");
+    }
+}
+
+void restore_from_history(int hist_idx) {
+    clear_current_input();
+    int i = 0;
+    while (command_history[hist_idx][i] != '\0' && i < BUFFER_SIZE - 1) {
+        input_buffer[i] = command_history[hist_idx][i];
+        char buf[2] = {command_history[hist_idx][i], '\0'};
+        print_to_screen(buf);
+        i++;
+    }
+    buffer_index = i;
+    input_buffer[buffer_index] = '\0';
+}
+
 void keyboard_handler() {
     uint8_t scancode = inb(KBD_DATA_PORT);
 
@@ -63,6 +89,32 @@ void keyboard_handler() {
     }
 
     if (!(scancode & KBD_SCANCODE_RELEASE)) {
+        if (scancode == 0x48) {
+            if (history_count > 0) {
+                if (!browsing_history) {
+                    history_index = history_count - 1;
+                    browsing_history = 1;
+                } else if (history_index > 0) {
+                    history_index--;
+                }
+                restore_from_history(history_index);
+            }
+            return;
+        }
+        
+        if (scancode == 0x50) {
+            if (browsing_history) {
+                if (history_index < history_count - 1) {
+                    history_index++;
+                    restore_from_history(history_index);
+                } else {
+                    clear_current_input();
+                    browsing_history = 0;
+                }
+            }
+            return;
+        }
+
         char key = 0;
 
         if (shift_pressed) {
@@ -78,8 +130,24 @@ void keyboard_handler() {
         }
 
         if (key) {
+            browsing_history = 0;
+            
             if (key == '\n') {
                 input_buffer[buffer_index] = '\0';
+                
+                if (buffer_index > 0) {
+                    int hist_slot = history_count % HISTORY_SIZE;
+                    int i = 0;
+                    while (input_buffer[i] != '\0' && i < BUFFER_SIZE - 1) {
+                        command_history[hist_slot][i] = input_buffer[i];
+                        i++;
+                    }
+                    command_history[hist_slot][i] = '\0';
+                    if (history_count < HISTORY_SIZE) {
+                        history_count++;
+                    }
+                }
+                
                 int i = 0;
                 while (input_buffer[i] != '\0' && i < BUFFER_SIZE) {
                     input_line[i] = input_buffer[i];
